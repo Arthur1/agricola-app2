38a38eda45c7f5cfdfa8c3184634bbd761ec1baf
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2020-02-07 02:56:53 +0900

    install package

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'Randomizer for Agricola',
+    themeColor: '#FE9700',
+    msTileColor: '#FE9700',
+    manifestOptions: {
+      background_color: '#FE9700'
+    }
+  }
+}
