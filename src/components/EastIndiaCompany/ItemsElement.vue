b6bf53e9bf7adb65b5006d1a0e5b012e4f329c7a
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2020-02-07 07:21:58 +0900

    East India Company

---
+++ src/components/EastIndiaCompany/ItemsElement.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="items_item">
+    <img :src="item.src" :alt="item.name" :class="{ 'items_item_icon': true, 'items_item_icon-picked': isPicked }">
+  </div>
+</template>
+<script>
+export default {
+  props: [
+    'item',
+    'pickedItemsList'
+  ],
+  computed: {
+    isPicked: function() {
+      return this.pickedItemsList.includes(this.item.id)
+    }
+  }
+}
+</script>
+<style scoped>
+.items_item {
+  max-width: 3em;
+  padding: 0.2em;
+}
+.items_item_icon {
+  width: 100%;
+  opacity: 1;
+}
+.items_item_icon-picked {
+  opacity: 0.25;
+}
+</style>
--- src/views/EastIndiaCompany.vue
+++ src/views/EastIndiaCompany.vue
@@ -5,11 +5,131 @@
       <p>
         各ラウンドのはじめに「品物を引く」ボタンを押してください。
       </p>
+      <div class="text-center">
+        <b-button variant="info" class="button-pickup" @click="pickItem" :disabled="unpickedItemsList.length === 0"><b-icon-box-arrow-up/>品物を引く</b-button>
+        <b-button variant="danger" v-b-modal.resetModal><b-icon-x-circle/>リセット</b-button>
+        <b-modal id="resetModal" title="リセット" cancel-title="キャンセル" ok-variant="danger" ok-title="リセットする" @ok="resetItems">
+          <p class="my-4">この操作は取り消せません。本当によろしいですか。</p>
+        </b-modal>
+      </div>
+      <h2 class="mt-3 text-success">残りの品物</h2>
+      <div class="items">
+        <items-element v-for="item in items" :item="item" :pickedItemsList="pickedItemsList" :key="item.id" />
+      </div>
+      <h2 class="mt-3 text-success">履歴</h2>
+      <b-list-group>
+        <b-list-group-item v-for="(itemID, key) in pickedItemsList" :key="key">
+          {{ key + 1 }}回目:
+          <img class="history_icon" :src="items[itemID - 1].src">
+          <span>{{ items[itemID - 1].name }}を得ました</span>
+        </b-list-group-item>
+      </b-list-group>
     </b-container>
   </div>
 </template>
 <script>
+import ItemsElement from '@/components/EastIndiaCompany/ItemsElement.vue'
+
+const localStorageName = 'east_india_company_items'
+
 export default {
-  
+  name: 'EastIndiaCompany',
+  components: {
+    ItemsElement
+  },
+  data: function() {
+    return {
+      items: [
+        {
+          id: 1,
+          name: '小麦',
+          src: require('@/assets/img/grain.png')
+        },
+        {
+          id: 2,
+          name: '野菜',
+          src: require('@/assets/img/vegetable.png')
+        },
+        {
+          id: 3,
+          name: '羊',
+          src: require('@/assets/img/sheep.png')
+        },
+        {
+          id: 4,
+          name: '猪',
+          src: require('@/assets/img/boar.png')
+        },
+        {
+          id: 5,
+          name: '牛',
+          src: require('@/assets/img/cattle.png')
+        },
+        {
+          id: 6,
+          name: '木材',
+          src: require('@/assets/img/wood.png')
+        },
+        {
+          id: 7,
+          name: 'レンガ',
+          src: require('@/assets/img/clay.png')
+        },
+        {
+          id: 8,
+          name: '葦',
+          src: require('@/assets/img/reed.png')
+        },
+        {
+          id: 9,
+          name: '石材',
+          src: require('@/assets/img/stone.png')
+        }
+      ],
+      pickedItemsList: []
+    }
+  },
+  computed: {
+    unpickedItemsList: function() {
+      let allItemsList = this.items.map(item => item.id)
+      return allItemsList.filter(id => ! this.pickedItemsList.includes(id), this)
+    }
+  },
+  created: function() {
+    this.pickedItemsList = JSON.parse(localStorage.getItem(localStorageName)) || []
+  },
+  methods: {
+    pickItem: function() {
+      let pickedItemID = this.unpickedItemsList[Math.floor(Math.random() * this.unpickedItemsList.length)]
+      this.pickedItemsList.push(pickedItemID)
+      localStorage.setItem(localStorageName, JSON.stringify(this.pickedItemsList))
+      this.$bvToast.toast(`${this.items[pickedItemID - 1].name}を得ました`, {
+        variant: 'success',
+        autoHideDelay: 1500
+      })
+    },
+    resetItems: function() {
+      this.pickedItemsList = []
+      localStorage.removeItem(localStorageName)
+      this.$bvToast.toast('リセットしました', {
+        variant: 'danger',
+        autoHideDelay: 1500
+      })
+    }
+  }
 }
 </script>
+<style scoped>
+.button-pickup {
+  margin-right: 1em;
+}
+.items {
+  display: flex;
+  flex-wrap: no-wrap;
+  justify-content: center;
+}
+.history_icon {
+  height: 1.5rem;
+  margin: 0 0.5rem;
+}
+</style>
