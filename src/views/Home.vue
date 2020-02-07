6e609d2162a1878acce2ea48df4e009368161f24
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2020-02-08 08:48:43 +0900

    Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
     <b-container>
-      <h1 class="mt-5 text-primary">Agricola for Randomizer</h1>
+      <b-jumbotron header-level="5" class="mt-3" lead="「教養特論：アグリコラ拡張学」付録">
+        <template v-slot:header>Randomizer for Agricola</template>
+        <hr class="my-2">
+        <p>拡張アグリコラのためのランダマイザWebアプリ</p>
+      </b-jumbotron>
+      <h2 class="text-success">新機能追加</h2>
       <p>
-        「教養特論：拡張アグリコラ学」を読んでいただきありがとうございます。
+        2018年11月に頒布した「教養特論：アグリコラ拡張学」の付録として公開した本アプリですが、2020年2月にリニューアルしました。
+      </p>
+      <ul>
+        <li>「ホモ・ルーデンス」の追加</li>
+        <li>「乱数生成」の追加</li>
+        <li>デザインの改良</li>
+      </ul>
+      <p>
+        また、PWA(Progressive Web Apps)に対応し、オフラインでも利用できるようになりました。ぜひホーム画面に追加してみてください。
+      </p>
+      <h2 class="text-success">お問い合わせ</h2>
+      <p>
+        本アプリに関するお問い合わせは、Arthur(<a href="https://twitter.com/Arthur1__" target="_blank" rel="noopener">@Arthur1__</a>)までお寄せください。
       </p>
     </b-container>
   </div>
