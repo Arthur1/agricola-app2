79e3cda650710ceb6d6773759e2fcb446656f215
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2020-02-08 08:25:43 +0900

    Veterinarian

---
+++ src/components/Veterinarian/HistoryElement.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-list-group-item :variant="order === 0 ? 'info' : ''">
+    {{ length - order }}回目:
+    <span class="history_icon">
+      <img :src="marker1.src" :alt="marker1.name">・<img :src="marker2.src" :alt="marker2.name">
+    </span>
+    <br>
+    <span v-if="marker1.id === marker2.id">
+      <span class="history_icon">
+        <img :src="marker1.animalSrc" :alt="marker1.animalName">
+      </span>
+      {{ marker1.animalName }}
+    </span>
+    <span v-else>
+      ---
+    </span>
+  </b-list-group-item>
+</template>
+<script>
+export default {
+  props: [
+    'order',
+    'pair',
+    'markers',
+    'length'
+  ],
+  computed: {
+    marker1: function() {
+      return this.markers[this.pair[0]]
+    },
+    marker2: function() {
+      return this.markers[this.pair[1]]
+    }
+  }
+}
+</script>
+<style scoped>
+.history_icon {
+  height: 1.5rem;
+  margin: 0 0.5rem;
+}
+.history_icon img {
+  height: 1.5rem;
+}
+</style>
--- src/views/Veterinarian.vue
+++ src/views/Veterinarian.vue
@@ -5,11 +5,160 @@
       <p>
         各ラウンドのはじめに「マーカーを引く」ボタンを押してください。
       </p>
+      <div class="text-center">
+        <b-button variant="info" class="button-pickup" @click="pickMarkers" :disabled="markersList.length < 2"><b-icon-box-arrow-up/>マーカーを引く</b-button>
+        <b-button variant="danger" v-b-modal.resetModal><b-icon-x-circle/>リセット</b-button>
+        <b-modal id="resetModal" title="リセット" cancel-title="キャンセル" ok-variant="danger" ok-title="リセットする" @ok="resetMarkers">
+          <p class="my-4">この操作は取り消せません。本当によろしいですか。</p>
+        </b-modal>
+      </div>
+      <h2 class="mt-3 text-success">残りのマーカー</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th v-for="marker in markers" :key="marker.id" scope="col" class="text-center">
+              <img :src="marker.src" :alt="marker.name" class="markers_icon">
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td v-for="marker in markers" :key="marker.id" scope="row" class="text-center">
+              {{ marker.number }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h2 class="mt-3 text-success">確率</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th v-for="marker in markers" :key="marker.id" scope="col" class="text-center">
+              <img :src="marker.animalSrc" :alt="marker.animalName" class="markers_icon">
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td v-for="marker in markers" :key="marker.id" scope="row" class="text-center">
+              {{ probabilities[marker.id] | percentage }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h2 class="mt-3 text-success">履歴</h2>
+      <b-list-group>
+        <history-element v-for="(pair, key) in reversedHistory" :key="key" :pair="pair" :markers="markers" :length="reversedHistory.length" :order="key" />
+      </b-list-group>
     </b-container>
   </div>
 </template>
 <script>
+import HistoryElement from '@/components/Veterinarian/HistoryElement.vue'
+const localStorageName = 'veterinarian_history'
 export default {
-  
+  name: 'Veterinarian',
+  components: {
+    HistoryElement
+  },
+  data: function() {
+    return {
+      markers: [
+        {
+          id: 0,
+          name: '葦',
+          src: require('@/assets/img/reed.png'),
+          animalName: '羊',
+          animalSrc: require('@/assets/img/sheep.png'),
+          number: 4
+        },
+        {
+          id: 1,
+          name: '石材',
+          src: require('@/assets/img/stone.png'),
+          animalName: '猪',
+          animalSrc: require('@/assets/img/boar.png'),
+          number: 3
+        },
+        {
+          id: 2,
+          name: '木材',
+          src: require('@/assets/img/wood.png'),
+          animalName: '牛',
+          animalSrc: require('@/assets/img/cattle.png'),
+          number: 2
+        }
+      ],
+      history: []
+    }
+  },
+  computed: {
+    markersList: function() {
+      let list = []
+      this.markers.forEach(marker => [...Array(marker.number)].map(() => list.push(marker.id)))
+      return list
+    },
+    reversedHistory: function() {
+      return this.history.slice().reverse()
+    },
+    probabilities: function() {
+      let markersTotal = this.markersList.length
+      return this.markers.map(marker => {
+        return marker.number * (marker.number - 1) / markersTotal / (markersTotal - 1)
+      })
+    }
+  },
+  created: function() {
+    this.history = JSON.parse(localStorage.getItem(localStorageName)) || []
+    this.history.forEach(pair => {
+      if (pair[0] === pair[1]) this.markers[pair[0]].number--
+    })
+  },
+  methods: {
+    pickMarkers: function() {
+      let pickedMarker1 = this.markers[this.getRandomElement(this.markersList)]
+      pickedMarker1.number--
+      let pickedMarker2 = this.markers[this.getRandomElement(this.markersList)]
+      this.history.push([pickedMarker1.id, pickedMarker2.id])
+      localStorage.setItem(localStorageName, JSON.stringify(this.history))
+      if (pickedMarker1.id === pickedMarker2.id) {
+        this.$bvToast.toast(`${pickedMarker1.animalName}を得ました`, {
+          variant: 'success',
+          autoHideDelay: 1500
+        })
+      } else {
+        pickedMarker1.number++
+        this.$bvToast.toast('何も得られませんでした', {
+          variant: 'warning',
+          autoHideDelay: 1500
+        })
+      }  
+    },
+    resetMarkers: function() {
+      this.markers[0].number = 4
+      this.markers[1].number = 3
+      this.markers[2].number = 2
+      this.history = []
+      localStorage.removeItem(localStorageName, this.history)
+      this.$bvToast.toast('リセットしました', {
+        variant: 'danger',
+        autoHideDelay: 1500
+      })
+    }
+  },
+  filters: {
+    percentage: function(decimal) {
+      return Math.round(decimal * 100) + '%'
+    }
+  }
+
 }
 </script>
+<style scoped>
+.button-pickup {
+  margin-right: 1em;
+}
+.markers_icon {
+  height: 1.5rem;
+}
+</style>
